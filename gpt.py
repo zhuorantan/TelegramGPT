341deab50b53e05751621835e8453aeb0c138655
Zhuoran Tan <t@zhuorantan.com>
2023-03-20 09:25:39 +0800

    improve timing of title generation

--- gpt.py
+++ gpt.py
@@ -10,7 +10,7 @@ class GPTClient:
     self.__max_message_count = max_message_count
 
     openai.api_key = api_key
-    openai.aiosession.set(ClientSession(trust_env=True, timeout=30))
+    openai.aiosession.set(ClientSession(trust_env=True))
 
   async def complete(self, conversation: Conversation, user_message: UserMessage, sent_msg_id: int, system_message: SystemMessage|None) -> AssistantMessage:
     logging.info(f"Completing message for conversation {conversation.id}, message: '{user_message}'")
@@ -24,21 +24,22 @@ class GPTClient:
 
     logging.info(f"Completed message for chat {conversation.id}, message: '{assistant_message}'")
 
+    if conversation.title is None and len(conversation.messages) < 3:
+      async def set_title(conversation: Conversation):
+        prompt = 'You are a title generator. You will receive one or multiple messages of a conversation. You will reply with only the title of the conversation without any punctuation mark either at the begining or the end.'
+        messages = [SystemMessage(prompt)] + conversation.messages
+
+        title = await self.__request(messages)
+        conversation.title = title
+
+        logging.info(f"Set title for conversation {conversation}: '{title}'")
+
+      asyncio.create_task(set_title(conversation))
+
     return assistant_message
 
   def new_conversation(self, conversation_id: int, user_message: UserMessage) -> Conversation:
     conversation = Conversation(conversation_id, None, user_message.timestamp, [user_message])
-
-    async def set_title(conversation: Conversation):
-      prompt = 'You are a title generator. You will receive one or multiple messages of a conversation. You will reply with only the title of the conversation without any punctuation mark either at the begining or the end.'
-      messages = [SystemMessage(prompt)] + conversation.messages
-
-      title = await self.__request(messages)
-      conversation.title = title
-
-      logging.info(f"Set title for conversation {conversation}: '{title}'")
-
-    asyncio.create_task(set_title(conversation))
 
     if self.__max_message_count and len(conversation.messages) > self.__max_message_count:
       conversation.messages = conversation.messages[self.__max_message_count:]
@@ -46,8 +47,9 @@ class GPTClient:
     return conversation
 
   async def __request(self, messages: list[Message]) -> str:
-    response = await openai.ChatCompletion.acreate(
+    task = openai.ChatCompletion.acreate(
       model='gpt-3.5-turbo',
       messages=[{'role': message.role, 'content': message.content} for message in messages],
     )
+    response = await asyncio.wait_for(task, 30)
     return cast(dict, response)['choices'][0]['message']['content']
